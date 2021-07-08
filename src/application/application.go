b4b2bd716234b88f176b5a883e73c1d1a26f92bf
Dragan Ćulibrk <draganculibrk9@uns.ac.rs>
2021-07-08 11:28:59 +0200

    #11 Add Prometheus metrics

--- src/application/application.go
+++ src/application/application.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Nistagram-Organization/nistagram-shared/src/utils/prometheus_handler"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/soheilhy/cmux"
 	"google.golang.org/grpc"
 	"log"
@@ -19,7 +20,10 @@ import (
 )
 
 var (
-	router = gin.Default()
+	router        = gin.Default()
+	requestsCount = prometheus_handler.GetHttpRequestsCounter()
+	requestsSize  = prometheus_handler.GetHttpRequestsSize()
+	uniqueUsers   = prometheus_handler.GetUniqueClients()
 )
 
 const (
@@ -30,11 +34,24 @@ const (
 	dockerKey       = "docker"
 )
 
-func StartApplication() {
+func configureCORS() {
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowAllOrigins = true
 	corsConfig.AddAllowHeaders("Authorization")
 	router.Use(cors.New(corsConfig))
+}
+
+func registerPrometheusMiddleware() {
+	prometheus.Register(requestsCount)
+	prometheus.Register(requestsSize)
+	prometheus.Register(uniqueUsers)
+
+	router.Use(prometheus_handler.PrometheusMiddleware(requestsCount, requestsSize, uniqueUsers))
+}
+
+func StartApplication() {
+	configureCORS()
+	registerPrometheusMiddleware()
 
 	domain := os.Getenv(domainKey)
 	clientId := os.Getenv(clientIdKey)
